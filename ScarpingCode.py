b3eb4590ab51bc9814cbcdb83d050822838e0f79
Shashank Mysore Bhagwan <34459426+shashankmb94@users.noreply.github.com>
2018-03-19 15:13:09 -0400

    Update ScarpingCode.py

--- ScarpingCode.py
+++ ScarpingCode.py
@@ -1,1 +1,3 @@
-#Code For Bestbuy  Site Reviews
+#Code For Web Scraping
+#Authors: Pratiba Dwivdi, Preeti Negi, Madhura, Shashank Mysore Bhagwan
+#We pledge our honor that we have abided by the Stevens Honor System
